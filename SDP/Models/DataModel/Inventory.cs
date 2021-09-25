c24330be68c0d1e384f5222e91104a45071b41e5
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2021-09-25 23:40:46 +1000

    Add display name for stockQty field.

--- SDP/Models/DataModel/Inventory.cs
+++ SDP/Models/DataModel/Inventory.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -19,6 +20,8 @@ namespace SDP.Models
 
         public Guid inventoryId { get; set; }
         public Product product { get; set; }
+
+        [Display(Name = "Quantity in Stock")]
         public int stockQty { get; set; }
     }
 }
