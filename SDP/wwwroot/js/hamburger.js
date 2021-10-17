ddb320cdc7adc029079854f2660c5c894ce284cf
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2021-10-17 22:42:28 +1100

    Add basic hamburger toggle.

---
+++ SDP/wwwroot/js/hamburger.js
@@ -0,0 +1,10 @@
+﻿// Add an event listener that waits for a click on the hamburger icon.
+document.getElementById("nav-links-collapsed").addEventListener("click", toggleHamburgerContent);
+
+// Target the content to toggle when clicked.
+var h = document.getElementById("nav-links-mobile");
+
+// Toggle visibility of hamburger content.
+function toggleHamburgerContent() {
+    h.style.display === "none" ? h.style.display = "block" : h.style.display = "none";
+}
