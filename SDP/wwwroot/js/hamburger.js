6d5f4eda68a307ea23d03614e2dbb0a116f39439
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2021-10-18 09:20:21 +1100

    Optimise CSS and JS for hamburger show / hide.

--- SDP/wwwroot/js/hamburger.js
+++ SDP/wwwroot/js/hamburger.js
@@ -1,21 +1,10 @@
 ﻿// Add an event listener that waits for a click on the hamburger icon.
 document.getElementById("nav-links-collapsed").addEventListener("click", toggleHamburgerContent);
 
-// Target the content to toggle when clicked.
-var h = document.getElementById("nav-links-mobile");
-// This variable avoids the conditional statement from evaluating incorrectly
-// on the first click. Another workaround is to add display: none as inline
-// styling (prefer to avoid this).
-var hContentVisible = false;
+// Define the class name to target for display / hide.
+var hClass = document.getElementById("nav-links-mobile").className;
 
 // Toggle visibility of hamburger content.
 function toggleHamburgerContent() {
-    if (!hContentVisible) {
-        h.style.display = "block";
-        hContentVisible = true;
-    }
-    else {
-        h.style.display = "none";
-        hContentVisible = false;
-    }
+    hClass === "hide" ? hClass = "show" : hClass = "hide";
 }
