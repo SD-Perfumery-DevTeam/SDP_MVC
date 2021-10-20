7e9f04ddf4b22bf05ac7795ba4617fb0d145db8f
Tim X <80139645+pengbo-XUE@users.noreply.github.com>
2021-10-20 19:48:51 +1100

    payment gate updated

--- SDPCore/Models/AccountModel/Key.cs
+++ SDPCore/Models/AccountModel/Key.cs
@@ -8,21 +8,25 @@ namespace SDPCore.Models.AccountModel
 {
     public class Key
     {
-        public Key(string publicKey, string descirption, decimal? amount, string currency)
+   
+
+        public Key()
+        {
+        }
+
+        public Key(string publicKey, string descirption, decimal? amount, string currency, string email)
         {
             this.publicKey = publicKey;
             this.descirption = descirption;
             this.amount = amount;
             this.currency = currency;
-        }
-
-        public Key()
-        {
+            this.email = email;
         }
 
         public string publicKey { get; set; }
         public string descirption { get; set; }
         public decimal? amount { get; set; }
         public string currency { get; set; }
+        public string email { get; set; }
     }
 }
