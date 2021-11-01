dc011c52c63e66c50ad434f4d2d78db3665fc49f
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2021-11-01 22:05:38 +1100

    Rename Article model.

---
+++ SDPCore/Models/DataModel/Article.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microsoft.SDP.SDPCore.Models
+{
+    public class Article
+    {
+        public Guid articleId { get; set; }
+        public string title { get; set; }
+        public string imgUrl { get; set; }
+        public string text { get; set; }
+    }
+}
