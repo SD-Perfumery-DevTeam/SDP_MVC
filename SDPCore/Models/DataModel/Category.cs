d7f8dcc2b22db59dc981f26291b3a4a674c49095
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2021-11-01 22:11:19 +1100

    Update Category model.

--- SDPCore/Models/DataModel/Category.cs
+++ SDPCore/Models/DataModel/Category.cs
@@ -1,6 +1,6 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web.Mvc;
@@ -9,18 +9,41 @@ namespace Microsoft.SDP.SDPCore.Models
 {
     public class Category
     {
+        public Guid categoryId { get; set; }
+
+        [Required]
+        [StringLength(256)]
+        [Display(Name = "Title")]
+        public string title{ get; set; }
+
+        [StringLength(2048)]
+        [Display(Name = "Category Image")]
+        public string imgUrl { get; set; }
+
+        [StringLength(2048)]
+        [Display(Name = "Title")]
+        public string text { get; set; }
+
+        // Default constructor
+        public Category() { }
+
+        // Constructor without specifying a categoryId
         public Category( string title, string imgUrl, string text)
         {
-            this.categoryId = Guid.NewGuid();
+            this.categoryId = categoryId;
             this.title = title;
             this.imgUrl = imgUrl;
             this.text = text;
         }
 
-        public Guid categoryId { get; set; }
-        public string title{ get; set; }
-        public string imgUrl { get; set; }
-        public string text { get; set; }
+        // Constructor specifying a categoryId
+        public Category( Guid categoryId, string title, string imgUrl, string text)
+        {
+            this.categoryId = categoryId;
+            this.title = title;
+            this.imgUrl = imgUrl;
+            this.text = text;
+        }
 
         public static explicit operator Category(SelectListItem v)
         {
