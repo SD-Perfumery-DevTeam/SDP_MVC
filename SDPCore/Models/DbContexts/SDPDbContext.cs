bd965e0a33d17f1190775c062daf86c9abaa2652
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2021-11-01 22:05:11 +1100

    Update DbContexts.

--- SDPCore/Models/DbContexts/SDPDbContext.cs
+++ SDPCore/Models/DbContexts/SDPDbContext.cs
@@ -9,19 +9,19 @@ namespace Microsoft.SDP.SDPCore.Models.DbContexts
 {
     public class SDPDbContext:IdentityDbContext
     {
-        public DbSet<Product> product { get; set; }
-        public DbSet<Category> category { get; set; }
+        public DbSet<Article> article { get; set; }
         public DbSet<Brand> brand { get; set; }
+        public DbSet<Category> category { get; set; }
+        public DbSet<Delivery> delivery { get; set; }
         public DbSet<Inventory> inventory{ get; set; }
-        public DbSet<Promotion> promotion { get; set; }
         public DbSet<Order> order { get; set; }
         public DbSet<OrderLine> orderLine { get; set; }
-        public DbSet<Delivery> delivery { get; set; }
+        public DbSet<Product> product { get; set; }
+        public DbSet<Promotion> promotion { get; set; }
         /*  
           public DbSet<Customer> customers { get; set; }
           public DbSet<RegisteredCustomer> registeredCustomers { get; set; }
-          public DbSet<Articles> articles { get; set; }
-          */
+        */
 
         public SDPDbContext(DbContextOptions<SDPDbContext> options):base(options)
         {
