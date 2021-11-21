012885842ae23b013ef7b49363d9ece36a291bb3
TroysProgramming <troysprogramming@gmail.com>
2021-11-21 15:13:09 +1100

    example commit

--- SDPCore/Models/DataModel/Product.cs
+++ SDPCore/Models/DataModel/Product.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 namespace Microsoft.SDP.SDPCore.Models
 {
 
+    // Example commit
+
     //this is the product model.... duh...
     [Serializable]
     public class Product
