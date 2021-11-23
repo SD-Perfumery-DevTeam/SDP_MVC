5699ac30a3b3aa9095697d04d9600c42c0a20602
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2021-11-23 13:40:43 +1100

    Using System.Reflection .

--- SDPCore/Consts.cs
+++ SDPCore/Consts.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace Microsoft.SDP.SDPCore
